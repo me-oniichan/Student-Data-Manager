b067a475d40730803f4dd24af88cd71069ee482f
Codevs-py <66708802+Codevs-py@users.noreply.github.com>
2022-07-13 18:49:29 +0530

    added bootstrap

--- src/Component/Body.jsx
+++ src/Component/Body.jsx
@@ -1,32 +1,34 @@
 export default function Body() {
     return (
-        <div className="container">
-            <table>
-                <thead>
-                    <tr>
-                        <th style={{ borderLeft: "0" }}>Name</th>
-                        <th>Result</th>
-                        <th>Math</th>
-                        <th>Chemistry</th>
-                        <th>Physics</th>
-                        <th>English</th>
-                        <th>Computer</th>
-                        <th style={{ borderRight: "None" }}>Remark</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td style={{ borderLeft: "0" }}>Dummy</td>
-                        <td>pass</td>
-                        <td className="shift-right">90</td>
-                        <td className="shift-right">80</td>
-                        <td className="shift-right">80</td>
-                        <td className="shift-right">80</td>
-                        <td className="shift-right">80</td>
-                        <td style={{ borderRight: "None" }}>Excellent</td>
-                    </tr>
-                </tbody>
-            </table>
+        <div className="container-fluid">
+            <div className="table-responsive-xl">
+                <table className="table table-striped table-hover table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Result</th>
+                            <th>Math</th>
+                            <th>Chemistry</th>
+                            <th>Physics</th>
+                            <th>English</th>
+                            <th>Computer</th>
+                            <th>Remark</th>
+                        </tr>
+                    </thead>
+                    <tbody className="table-group-divider">
+                        <tr>
+                            <td>Dummy</td>
+                            <td>pass</td>
+                            <td className="shift-right">90</td>
+                            <td className="shift-right">80</td>
+                            <td className="shift-right">80</td>
+                            <td className="shift-right">80</td>
+                            <td className="shift-right">80</td>
+                            <td>Excellent</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
