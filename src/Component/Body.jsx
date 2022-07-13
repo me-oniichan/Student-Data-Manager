59659d4b1a742ae7bae2313053447ce2a9625f28
Codevs-py <66708802+Codevs-py@users.noreply.github.com>
2022-07-13 18:29:01 +0530

    added table

--- src/Component/Body.jsx
+++ src/Component/Body.jsx
@@ -4,26 +4,26 @@ export default function Body() {
             <table>
                 <thead>
                     <tr>
-                        <th>Name</th>
+                        <th style={{ borderLeft: "0" }}>Name</th>
                         <th>Result</th>
                         <th>Math</th>
                         <th>Chemistry</th>
                         <th>Physics</th>
                         <th>English</th>
                         <th>Computer</th>
-                        <th>Remark</th>
+                        <th style={{ borderRight: "None" }}>Remark</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <td>Dummy</td>
+                        <td style={{ borderLeft: "0" }}>Dummy</td>
                         <td>pass</td>
                         <td className="shift-right">90</td>
                         <td className="shift-right">80</td>
                         <td className="shift-right">80</td>
                         <td className="shift-right">80</td>
                         <td className="shift-right">80</td>
-                        <td>Excellent</td>
+                        <td style={{ borderRight: "None" }}>Excellent</td>
                     </tr>
                 </tbody>
             </table>
