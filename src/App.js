4730389b8e3705fd3f396d011ad8da23ade35485
Codevs-py <66708802+Codevs-py@users.noreply.github.com>
2022-07-13 11:41:20 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Navbar from './Component/Navbar';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Routes>
+          <Route path='/' element={<Navbar/>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
