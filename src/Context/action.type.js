b8a1db5ad68c6b1976509c25f7af10dffbed4dde
Codevs-py <66708802+Codevs-py@users.noreply.github.com>
2022-07-14 19:10:23 +0530

    added context and reducers

---
+++ src/Context/action.type.js
@@ -0,0 +1,4 @@
+export const SET_LOADING = "SET_LOADING";
+export const SET_DATA = "SET_DATA";
+export const DATA_TO_UPDATE = "DATA_TO_UPDATE";
+export const CURRENT_DATA = "CURRENT_DATA";
---
+++ src/Context/reducer.js
@@ -0,0 +1,14 @@
+import { CURRENT_DATA, DATA_TO_UPDATE, SET_DATA, SET_LOADING } from "./action.type";
+
+export default (state, action) =>{
+    switch (action.type){
+        case SET_DATA:
+            return
+        case SET_LOADING:
+            return
+        case DATA_TO_UPDATE:
+            return
+        case CURRENT_DATA:
+            return
+    }
+}
